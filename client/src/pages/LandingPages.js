6f89b1aa201b984e4656d2ff5e8cdc01110be06f
vickel leung <>
2020-01-18 17:32:59 -0500

    no message

---
+++ client/src/pages/LandingPages.js
@@ -0,0 +1,13 @@
+import React, {Component} from "react";
+
+class LandingPages extends Component{
+    render(){
+        return(
+            <div>
+
+            </div>
+        )
+    }
+}
+
+export {LandingPages};
