46c2b910cff38ba1da575601163dd2e30d5fef01
vickel leung <>
2020-01-18 17:32:29 -0500

    added initials pages

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,28 @@
 import React from 'react';
+import styled from "styled-components";
+
 
 function App() {
   return (
     <div className="App">
      <div>
        <p>Homepage</p>
+
+       <Wrapper>
+         <Title>Hello there</Title>
+       </Wrapper>
+       
      </div>
     </div>
   );
 }
 
 export default App;
+
+const Wrapper = styled.div`
+  border: 2px solid black;
+`;
+
+const Title = styled.h1`
+  color:red;
+`;
